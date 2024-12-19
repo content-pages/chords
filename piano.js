5c69b9fc333ce8e592281fc475b922918a10cfc7
content-pages <161290566+content-pages@users.noreply.github.com>
2024-12-19 21:28:36 +0700

    Create piano.js

---
+++ piano.js
@@ -0,0 +1,133 @@
+piano = {
+	Chords : {
+		C	: ["C-2"  , "E-2"  , "G-2"  ],
+		Cm      : ["C-2"  , "Ds-2" , "G-2"  ],
+		C6	: ["C-2"  , "E-2"  , "A-2"  ],
+		Cm6	: ["C-2"  , "Ds-2" , "A-2"  ],
+		C69	: ["A-1"  , "C-2"  , "D-2" , "G-2"  ],
+		C7      : ["C-2"  , "E-2"  , "G-2" , "As-2" ],
+		Cm7	: ["C-2"  , "Ds-2" , "G-2" , "As-2" ],
+		"Cm7b5"	: ["C-2"  , "Ds-2" , "Fs-2", "As-2" ],
+		"C7b9"	: ["C-2"  , "E-2"  , "G-2" , "As-2", "Cs-2"  ],
+		C9	: ["C-2"  , "E-2"  , "G-2" , "As-2", "D-3"   ],
+		Cm9	: ["C-2"  , "Ds2"  , "G-2" , "As-2", "D-3"   ],
+		Cadd9	: ["C-2"  , "E-2"  , "G-2" , "D-3"  ],
+		C13	: ["Bb-1" , "C-2"  , "D-2" , "E-2",  "A-2"   ],
+		Csus2	: ["C-2"  , "D-2"  , "G-2"  ],
+		Csus4	: ["C-2"  , "F-2"  , "G-2"  ],
+		Cdim	: ["C-2"  , "Ds-2" , "Fs-2" ],
+		Cdim7	: ["C-2"  , "Ds-2" , "Fs-2", "A-2" ],
+		Caug	: ["C-2"  , "E-2"  , "Gs-2" ],
+		"C/E"	: ["E-2"  , "G-2"  , "C-3"  ],
+		"C/G"	: ["G-2"  , "C-3"  , "E-3"  ],
+		"C/D"	: ["D-2"  , "E-2"  , "G-2" , "C-3"  ],
+		"C/As"	: ["As-1" , "C-2"  , "E-2" , "G-2"],
+		
+		"Cs"	: ["Cs-2" , "F-2"  , "Gs-2" ],
+		"Csm"	: ["Cs-2" , "E-2"  , "Gs-2" ],
+		"Cs6"	: ["Cs-2" , "F-2"  , "As-2" ],
+		"Csm6"	: ["Cs-2" , "E-2"  , "As-2"  ],
+		"Cs69"	: ["As-1" , "Cs-2" , "Ds-2" , "Gs-2"  ],
+		"Cs7"   : ["Cs-2" , "F-2"  , "Gs-2" , "B-2" ],
+		"Csm7"	: ["Cs-2" , "E-2"  , "Gs-2" , "B-2" ],
+		"Csm7b5": ["Cs-2" , "E-2"  , "G-2", "B-2" ],
+		"Cs7b9"	: ["Cs-2" , "F-2"  , "Gs-2" , "B-2", "D-2"  ],
+		"Cs9"	: ["Cs-2" , "F-2"  , "Gs-2" , "B-2", "Ds-3"   ],
+		"Csm9"	: ["Cs-2" , "E-2"  , "Gs-2" , "B-2", "Ds-3"   ],
+		"Csadd9": ["Cs-2" , "F-2"  , "Gs-2" , "Ds-3"  ],
+		"Cs13"	: ["B-1"  , "Cs-2" , "Ds-2" , "F-2",  "As-2"   ],
+		"Cssus2": ["Cs-2" , "Ds-2" , "Gs-2"  ],
+		"Cssus4": ["Cs-2" , "Fs-2" , "Gs-2"  ],
+		"Csdim"	: ["Cs-2" , "E-2"  , "G-2" ],
+		"Csdim7": ["Cs-2" , "E-2"  , "G-2", "As-2" ],
+		"Csaug"	: ["Cs-2" , "F-2"  , "A-2" ],
+		"Cs/F"	: ["F-2"  , "Gs-2" , "Cs-3"  ],
+		"Cs/Gs"	: ["Gs-1" , "Cs-2" , "F-2"  ],
+		"Cs/Ds"	: ["Ds-2" , "F-2"  , "Gs-2" , "Cs-3"  ],
+		"Cs/B"	: ["B-1"  , "Cs-2" , "F-2" , "Gs-2"],
+		
+		"Db"	: ["Cs-2" , "F-2"  , "Gs-2" ],
+		"Dbm"	: ["Cs-2" , "E-2"  , "Gs-2" ],
+		"Db6"	: ["Cs-2" , "F-2"  , "As-2" ],
+		"Dbm6"	: ["Cs-2" , "E-2"  , "As-2"  ],
+		"Db69"	: ["As-1" , "Cs-2" , "Ds-2" , "Gs-2"  ],
+		"Dbs7"  : ["Cs-2" , "F-2"  , "Gs-2" , "B-2" ],
+		"Dbm7"	: ["Cs-2" , "E-2"  , "Gs-2" , "B-2" ],
+		"Dbm7b5": ["Cs-2" , "E-2"  , "G-2", "B-2" ],
+		"Db7b9"	: ["Cs-2" , "F-2"  , "Gs-2" , "B-2", "D-2"  ],
+		"Db9"	: ["Cs-2" , "F-2"  , "Gs-2" , "B-2", "Ds-3"   ],
+		"Dbm9"	: ["Cs-2" , "E-2"  , "Gs-2" , "B-2", "Ds-3"   ],
+		"Dbadd9": ["Cs-2" , "F-2"  , "Gs-2" , "Ds-3"  ],
+		"Db13"	: ["B-1"  , "Cs-2" , "Ds-2" , "F-2",  "As-2"   ],
+		"Dbsus2": ["Cs-2" , "Ds-2" , "Gs-2"  ],
+		"Dbsus4": ["Cs-2" , "Fs-2" , "Gs-2"  ],
+		"Dbdim"	: ["Cs-2" , "E-2"  , "G-2" ],
+		"Dbdim7": ["Cs-2" , "E-2"  , "G-2", "As-2" ],
+		"Dbaug"	: ["Cs-2" , "F-2"  , "A-2" ],
+		"Db/F"	: ["F-2"  , "Gs-2" , "Cs-3"  ],
+		"Db/Gs"	: ["Gs-1" , "Cs-2" , "F-2"  ],
+		"Db/Ds"	: ["Ds-2" , "F-2"  , "Gs-2" , "Cs-3"  ],
+		"Db/B"	: ["B-1"  , "Cs-2" , "F-2" , "Gs-2"],
+		
+		D	: ["D-2"  , "Fs-2"  , "A-2"  ],
+		Dm      : ["D-2"  , "F-2"   , "A-2"  ],
+		
+		"Ds"	: ["Ds-2" , "G-2"   , "As-2" ],
+		"Dsm"	: ["Ds-2" , "Fs-2"  , "As-2" ],
+		
+		"Eb"	: ["Ds-2" , "G-2"   , "As-2" ],
+		"Ebm"	: ["Ds-2" , "Fs-2"  , "As-2" ],
+		
+		E	: ["E-2"  , "Gs-2"  , "B-2"  ],
+		Em      : ["E-2"  , "G-2"   , "B-2"  ],
+		
+		F	: ["F-2"  , "A-2"   , "C-3"  ],
+		Fm      : ["F-2"  , "Gs-2"  , "C-3"  ],
+		
+		"Fs"	: ["Fs-2" , "As-2"  , "Cs-3" ],
+		"Fsm"	: ["Fs-2" , "A-2"   , "Cs-3" ],		
+		
+		"Gb"	: ["Fs-2" , "As-2"  , "Cs-3" ],
+		"Gbm"	: ["Fs-2" , "A-2"   , "Cs-3" ],
+		
+		G	: ["G-2"  , "B-2"   , "D-3"  ],
+		Gm      : ["G-2"  , "As-2"  , "D-3"  ],
+		
+		"Gs"	: ["Gs-2" , "C-3"   , "Ds-3" ],
+		"Gsm"	: ["Gs-2" , "B-2"   , "Ds-3" ],
+		
+		"Ab"	: ["Gs-2" , "C-3"   , "Ds-3" ],
+		"Abm"	: ["Gs-2" , "B-2"   , "Ds-3" ],
+		
+		A	: ["A-2"  , "Cs-3"  , "E-3"  ],
+		Am      : ["A-2"  , "C-3"   , "E-3"  ],
+		
+		"As"	: ["As-1" , "D-2"  , "F-2" ],
+		"Asm"	: ["As-1" , "Cs-2" , "F-2" ],
+		
+		"Bb"	: ["As-1" , "D-2"  , "F-2" ],
+		"Bbm"	: ["As-1" , "Cs-2" , "F-2" ],
+		
+		B	: ["B-1"  , "Ds-2" , "Fs-2"  ],
+		Bm      : ["B-1"  , "D-2"  , "Fs-2"  ],
+	},
+	render: function(Chord,Variation){
+		var rendered = document.getElementsByClassName("circle");
+
+		while(rendered[0]){
+			rendered[0].parentNode.removeChild(rendered[0]);
+		}			
+		
+		/* Change */
+		if(piano.Chords[Chord] != undefined){
+			this.Chords[Chord].forEach(function(c){
+				jQuery("#" + c,".ui-tooltip-content").html("<p class='circle'></p>");
+			});
+		}
+	},
+	generatePiano: function(){
+		var piano = "<ul id='piano'><li><div class='anchor'><div id='C-1'></div></div></li> <li><div class='anchor'><div id='D-1'></div></div><span><div id='Cs-1'></div></span></li> <li><div class='anchor'><div id='E-1'></div></div><span><div id='Ds-1'></div></span></li> <li><div class='anchor'><div id='F-1'></div></div></li> <li><div class='anchor'><div id='G-1'></div></div><span><div id='Fs-1'></div></span></li> <li><div class='anchor'><div id='A-1'></div></div><span><div id='Gs-1'></div></span></li> <li><div class='anchor'><div id='B-1'></div></div><span><div id='As-1'></div></span></li> <li><div class='anchor'><div id='C-2'></div></div></li> <li><div class='anchor'><div id='D-2'></div></div><span><div id='Cs-2'></div></span></li> <li><div class='anchor'><div id='E-2'></div></div><span><div id='Ds-2'></div></span></li> <li><div class='anchor'><div id='F-2'></div></div></li> <li><div class='anchor'><div id='G-2'></div></div><span><div id='Fs-2'></div></span></li> <li><div class='anchor'><div id='A-2'></div></div><span><div id='Gs-2'></div></span></li> <li><div class='anchor'><div id='B-2'></div></div><span><div id='As-2'></div></span></li> <li><div class='anchor'><div id='C-3'></div></div></li> <li><div class='anchor'><div id='D-3'></div></div><span><div id='Cs-3'></div></span></li> <li><div class='anchor'><div id='E-3'></div></div><span><div id='Ds-3'></div></span></li></ul>";
+
+		return piano;
+	}
+}
